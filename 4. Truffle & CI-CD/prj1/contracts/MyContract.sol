6e28932170839b2bde99dd7c312f6931c6337392
Gregory Faruch <gregory.faruch@web-atrio.com>
2022-12-13 05:28:20 +0100

    update: truffle

--- 4. Truffle & CI-CD/prj1/contracts/MyContract.sol
+++ 4. Truffle & CI-CD/prj1/contracts/MyContract.sol
@@ -2,6 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract MyContract {
-  constructor() public {
-  }
+    uint public s_number;
+
+    function setNumber(uint _nbr) public {
+        s_number = _nbr;
+    }
 }
