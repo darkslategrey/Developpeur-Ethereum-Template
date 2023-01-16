5730a683874665199558142f2c101f56c87162ee
Gregory Faruch <gregory.faruch@web-atrio.com>
2023-01-16 14:48:24 +0100

    test push

--- front/constants.js
+++ front/constants.js
@@ -1,29 +1,31 @@
-export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const contractAddress = "0x6174e4498DD1eb4a423B530E1F87966CDCE3da90";
+
+// export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const abi = [
-    {
-      "inputs": [],
-      "name": "getNumber",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_number",
-          "type": "uint256"
-        }
-      ],
-      "name": "setNumber",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    }
-]
+  {
+    inputs: [],
+    name: "getNumber",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_number",
+        type: "uint256",
+      },
+    ],
+    name: "setNumber",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
